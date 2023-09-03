9641ff40334f5572e4b91ac637e87deeab636c12
JashonL <721695214@qq.com>
2023-09-03 18:04:00 +0800

    UI界面开始

---
+++ app/src/main/java/com/dxm/dxmcharge/ui/record/RecorderActivity.kt
@@ -0,0 +1,12 @@
+package com.dxm.dxmcharge.ui.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dxm.dxmcharge.R
+
+class RecorderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recorder)
+    }
+}
